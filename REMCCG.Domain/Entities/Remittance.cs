6466a68818cdedfafe489ea5b0dedf654d2dd078
Nenty OtoAbasi Nenty <notoabasi@vatebra.com>
2023-08-31 20:43:21 +0100

    Version 1

--- REMCCG.Domain/Entities/Remittance.cs
+++ REMCCG.Domain/Entities/Remittance.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace REMCCG.Domain.Entities
+﻿namespace REMCCG.Domain.Entities
 {
     public class Remittance
     {
         public int ID { get; set; }
-        public RemittanceType Type { get; set; } // Enum for different types of offerings
+        public string RemittanceType { get; set; } // Enum for different types of offerings
         public decimal Amount { get; set; }
         public DateTime Date { get; set; }
         public int MemberID { get; set; }
